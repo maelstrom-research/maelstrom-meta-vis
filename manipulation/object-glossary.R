91c51b73d1c41c6a70ff48db0c664841f467ef58
Andrey Koval <koval.andrey@gmail.com>
2017-07-19 22:45:51 -0400

    stable object glossary

--- manipulation/object-glossary.R
+++ manipulation/object-glossary.R
@@ -1,4 +1,5 @@
 # Glossary of Objects and Names
 
 # common_stem <- c("Study", "Start", "End")
-common_stem <- c("Study",	"Population",	"Data Collection Event",	"Start",	"End")
+common_stem_dce <- c("Study",	"Population",	"Data Collection Event",	"Start",	"End")
+common_stem     <- c("Study",		                                      "Start",	"End")
